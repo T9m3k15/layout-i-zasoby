d9ff783d1399d5430e14da4f411a982aec72d637
T9m3k15 <122088983+T9m3k15@users.noreply.github.com>
2023-09-24 15:17:21 +0200

    Dodaj pliki projektów.

---
+++ layout i zasoby/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace AppMainWindow
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Dodaj obsługę zdarzeń przycisków
+            Przycisk1.Click += Przycisk1_Click;
+            Przycisk2.Click += Przycisk2_Click;
+            Przycisk3.Click += Przycisk3_Click;
+        }
+
+        private void Przycisk1_Click(object sender, RoutedEventArgs e)
+        {
+            // Obsługa kliknięcia przycisku 1
+            MessageBox.Show("Kliknięto przycisk 1");
+        }
+
+        private void Przycisk2_Click(object sender, RoutedEventArgs e)
+        {
+            // Obsługa kliknięcia przycisku 2
+            MessageBox.Show("Kliknięto przycisk 2");
+        }
+
+        private void Przycisk3_Click(object sender, RoutedEventArgs e)
+        {
+            // Obsługa kliknięcia przycisku 3
+            MessageBox.Show("Kliknięto przycisk 3");
+        }
+    }
+}
